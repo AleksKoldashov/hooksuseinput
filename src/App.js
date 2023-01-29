218d7c199a0639f22216aeaf8702a19580be815e
AleksKoldashov <kapahdalli88@gmail.com>
2023-01-29 18:00:18 +0400

    adduseTheme

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from 'react'
 import './App.css';
 import useInput from './hooks/useInput'
 import useFetch from './hooks/useFetch';
+import useTheme from './hooks/useTheme'
+
 
 function App() {
  const inp = useInput('Hello World', true)  
   
  const {data, loading, error} = useFetch('https://www.reddit.com/r/news.json')
+ 
+const {theme, toggleTheme} = useTheme()
+ 
  if(loading) return 'Loadig ....'
 
  if(error) {
   console.log('error', error);
  }
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
+      <button onClick={toggleTheme}>
+      Toggle Theme
+      </button>
       <form>
         <input 
         {...inp}
