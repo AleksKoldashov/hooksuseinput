cb66c0f6e648be2dadb7fa5fd88cf85026bc1273
AleksKoldashov <kapahdalli88@gmail.com>
2023-01-29 15:51:32 +0400

    usefetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './App.css';
 import useInput from './hooks/useInput'
+import useFetch from './hooks/useFetch';
 
 function App() {
  const inp = useInput('Hello World', true)  
   
+ const {data, loading, error} = useFetch('https://www.reddit.com/r/news.json')
+ if(loading) return 'Loadig ....'
+
+ if(error) {
+  console.log('error', error);
+ }
   return (
     <div className="App">
       <form>
@@ -13,6 +20,7 @@ function App() {
         />
         {inp.error && <span style={{ color: 'red'}}>{inp.error}</span>}
       </form>
+      {JSON.stringify(data && data.dist)}
     </div>
   );
 }
